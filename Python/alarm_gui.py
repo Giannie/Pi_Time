178ce7632f21d8dff5ceb84362a27809edf88d72
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2014-05-16 19:10:29 +0100

    color

--- Python/alarm_gui.py
+++ Python/alarm_gui.py
@@ -21,7 +21,7 @@ import sys
 
 class PyApp(gtk.Window):
     def __init__(self):
-        self.clock_color = '"dark blue"'
+        self.clock_color = '"purple"'
         self.text_color = "'purple'"
         self.output = ""
         self.line2 = ""
