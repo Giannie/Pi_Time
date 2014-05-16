6a07084872417be68001cf897bd1e7dca0fc8b51
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2014-05-16 16:02:39 +0100

    added scripts

---
+++ usr local bin/alarmcron.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ -z "$(echo $PATH | grep /usr/local/bin)" ]; then
+    PATH=/usr/local/bin:$PATH
+fi
+
+plug_on.sh && plug_on.sh &
+python /home/pi/Pi_Time/Python/alarm_off.py
+mpc random on
+mpc load AlarmPlaylist.m3u
+mpc play
+mpc pause
+sleep 5
+mpc volume 82
+mpc play
+sleep 15
+mpc volume 82
+sleep 15
+mpc volume 84
+sleep 15
+mpc volume 86
+sleep 15
+mpc volume 88
+sleep 15
+mpc volume 90
+string="$(mpc | grep playing)"
+if [ -z "$string" ]
+    then
+        mpc load AlarmPlaylist.m3u
+        mpc volume 80
+        mpc play
+        sleep 15
+        mpc volume 82
+        sleep 15
+        mpc volume 84
+        sleep 15
+        mpc volume 86
+        sleep 15
+        mpc volume 88
+        sleep 15
+        mpc volume 90
+fi
+sleep 240
+mpc volume 85
+sleep 1560
+string="$(mpc | grep playing)"
+if [ "$string" ]; then
+	mpc idle
+	mpc pause
+#	python /home/pi/Adafruit-Raspberry-Pi-Python-Code/Adafruit_CharLCDPlate/weather.py
+	mpc play
+fi
+sleep 1800
+string="$(mpc | grep playing)"
+if [ "$string" ]; then
+	mpc idle
+fi
+mpc random off
+mpc stop
+mpc clear
+mpc volume 90
+plug_off.sh && plug_off.sh &
---
+++ usr local bin/plug_off.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh pi@192.168.0.19 /usr/local/bin/plug_off.sh > /dev/null
