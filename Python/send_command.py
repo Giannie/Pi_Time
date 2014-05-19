ea671b619a01d7df722eb0c4794a1d5b2fe24061
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2014-05-19 12:57:21 +0100

    added python scripts for socket connections

--- Python/send_command.py
+++ Python/send_command.py
@@ -5,7 +5,7 @@ if len(sys.argv) < 2 or sys.argv[1] not in ['on','off']:
     print "Argument must be on or off"
     exit(1)
 
-message = sys.arg[1]
+message = sys.argv[1]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.0.19', 8000))
