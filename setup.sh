279a2c164266b58689e0e8b083467013a246a1c6
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2014-06-14 11:17:42 +0100

    touchscreen fix

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ fi
 
 
 sudo apt-get update
-sudo apt-get install mpc python-dev python-rpi.gpio python-pip htop elinks evtest tslib libts-bin python-gtk2
+sudo apt-get install mpc python-dev python-rpi.gpio python-pip htop elinks evtest tslib libts-bin python-gtk2 xinput
 sudo pip install python-crontab requests
 mkdir logs
 mkdir logs/cron
@@ -36,6 +36,7 @@ wget http://adafruit-download.s3.amazonaws.com/libraspberrypi-dev-adafruit.deb
 wget http://adafruit-download.s3.amazonaws.com/libraspberrypi-doc-adafruit.deb
 wget http://adafruit-download.s3.amazonaws.com/libraspberrypi0-adafruit.deb
 wget http://adafruit-download.s3.amazonaws.com/raspberrypi-bootloader-adafruit-112613.deb
+wget http://adafruit-download.s3.amazonaws.com/xinput-calibrator_0.7.5-1_armhf.deb
 sudo dpkg -i -B *.deb
 
 sudo mv /usr/share/X11/xorg.conf.d/99-fbturbo.conf ./
@@ -46,7 +47,7 @@ append spi-bcm2708 /etc/modules
 append fbtft_device /etc/modules
 append rpi_power_switch /etc/modules
 
-append "options fbtft_device name=adafruitts rotate=90 frequency=32000000" /etc/modprobe.d/adafruit.conf
+append "options fbtft_device name=adafruitts rotate=270 frequency=32000000" /etc/modprobe.d/adafruit.conf
 append "options rpi_power_switch gpio_pin=23 mode=0" /etc/modprobe.d/adafruit.conf
 
 append "SUBSYSTEM==\"input\", ATTRS{name}==\"stmpe-ts\", ENV{DEVNAME}==\"*event*\", SYMLINK+=\"input/touchscreen\""  /etc/udev/rules.d/95-stmpe.rules
@@ -56,7 +57,7 @@ if [ -z "$( grep "echo 252 > /sys/class/gpio/export" /etc/rc.local )" ]; then
     append "echo 252 > /sys/class/gpio/export" /etc/rc.local
     append "echo 'out' > /sys/class/gpio/gpio252/direction" /etc/rc.local
     append "echo '1' > /sys/class/gpio/gpio252/value" /etc/rc.local
-    append "DISPLAY=:0.0 python /home/pi/screen_control.py &" /etc/rc.local
+    append "DISPLAY=:0.0 python /home/pi/Pi_time/Python/screen_control.py &" /etc/rc.local
     append "exit 0" /etc/rc.local
 fi
 
@@ -64,8 +65,13 @@ if [ -z "$( grep "dpms -nocursor" /etc/lightdm/lightdm.conf )" ]; then
     sudo sed -i s/'#xserver-command=X'/'xserver-command=X -s 0 dpms -nocursor'/ /etc/lightdm/lightdm.conf
 fi
 
+if [ -z "$( grep python /etc/xdg/lxsession/LXDE/autostart )" ]; then
+    sudo sed -i 's/^\([^#]\)/#\1/g' /etc/xdg/lxsession/LXDE/autostart
+    echo "@python ~/Pi_Time/Python/alarm_gui.py" | sudo tee -a /etc/xdg/lxsession/LXDE/autostart
+fi
+
 cd Pi_Time
-sudo cp /home/pi/usr\ local\ bin/* /usr/local/bin/
+sudo cp usr\ local\ bin/* /usr/local/bin/
 sudo cp 99-fbdev.conf /usr/share/X11/xorg.conf.d/
 
 
